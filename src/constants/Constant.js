70a63ca18886ffe9172034a2cda2fe0c796e0f90
Rohitjangid17 <jangidrohit817@gmail.com>
2024-05-05 00:14:38 +0530

    update constant add skills array

--- src/constants/Constant.js
+++ src/constants/Constant.js
@@ -1,3 +1,7 @@
+import HtmlIcon from '@mui/icons-material/Html';
+import CssIcon from '@mui/icons-material/Css';
+import JavascriptIcon from '@mui/icons-material/Javascript';
+
 export const navLinks = [
     {
         id: 1,
@@ -24,4 +28,78 @@ export const navLinks = [
         title: "Contact",
         path: "/contact"
     },
+]
+
+export const skills = [
+    {
+        id: 1,
+        icon: <HtmlIcon />,
+        skill: "HTML"
+    },
+    {
+        id: 2,
+        icon: <CssIcon />,
+        skill: "CSS"
+    },
+    {
+        id: 3,
+        icon: <JavascriptIcon/>,
+        skill: "SCSS"
+    },
+    {
+        id: 4,
+        icon: <JavascriptIcon/>,
+        skill: "Tailwind CSS"
+    },
+    {
+        id: 5,
+        icon: <JavascriptIcon/>,
+        skill: "Bootstrap"
+    },
+    {
+        id: 6,
+        icon: <JavascriptIcon/>,
+        skill: "Javascript"
+    },
+    {
+        id: 7,
+        icon: <JavascriptIcon/>,
+        skill: "Typescript"
+    },
+    {
+        id: 8,
+        icon: <JavascriptIcon/>,
+        skill: "Angular"
+    },
+    {
+        id: 9,
+        icon: <JavascriptIcon/>,
+        skill: "React.js"
+    },
+    {
+        id: 7,
+        icon: <JavascriptIcon/>,
+        skill: "Next.js"
+    },
+    {
+        id: 10,
+        icon: <JavascriptIcon/>,
+        skill: "JQuery"
+    },
+    {
+        id: 11,
+        icon: <JavascriptIcon/>,
+        skill: "C#"
+    },
+    {
+        id: 12,
+        icon: <JavascriptIcon/>,
+        skill: "ASP.NET Core"
+    },
+    {
+        id: 13,
+        icon: <JavascriptIcon/>,
+        skill: "MS SQL"
+    },
+
 ]
