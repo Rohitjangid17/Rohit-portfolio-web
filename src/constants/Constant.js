0d1f1558fbe0150eeadc52f3ebc486d6fe283f7e
Rohitjangid17 <jangidrohit817@gmail.com>
2024-05-09 20:42:29 +0530

    skills icon add

--- src/constants/Constant.js
+++ src/constants/Constant.js
@@ -1,5 +1,13 @@
-import htmlIcon from '../assets/Icons/html_icon.png';
-import cssIcon from '../assets/Icons/css_icon.webp';
+import htmlIcon from '../assets/Icons/html_icon.svg';
+import cssIcon from '../assets/Icons/css_icon.svg';
+import tailwindIcon from '../assets/Icons/tailwind_css_icon.svg';
+import bootstrapIcon from '../assets/Icons/bootstrap_icon.svg';
+import javascriptIcon from '../assets/Icons/javascript_icon.svg';
+import typescriptIcon from '../assets/Icons/typescript_icon.svg';
+import angularIcon from '../assets/Icons/angular_icon.svg';
+import reactIcon from '../assets/Icons/react_icon.svg';
+import jqueryIcon from '../assets/Icons/jquery_icon.svg';
+import csharpIcon from '../assets/Icons/csharp_icon.svg';
 
 export const navLinks = [
     {
@@ -47,32 +55,32 @@ export const skills = [
     },
     {
         id: 4,
-        icon: htmlIcon,
+        icon: tailwindIcon,
         skill: "Tailwind CSS"
     },
     {
         id: 5,
-        icon: htmlIcon,
+        icon: bootstrapIcon,
         skill: "Bootstrap"
     },
     {
         id: 6,
-        icon: htmlIcon,
+        icon: javascriptIcon,
         skill: "Javascript"
     },
     {
         id: 7,
-        icon: htmlIcon,
+        icon: typescriptIcon,
         skill: "Typescript"
     },
     {
         id: 8,
-        icon: htmlIcon,
+        icon: angularIcon,
         skill: "Angular"
     },
     {
         id: 9,
-        icon: htmlIcon,
+        icon: reactIcon,
         skill: "React.js"
     },
     {
@@ -82,12 +90,12 @@ export const skills = [
     },
     {
         id: 10,
-        icon: htmlIcon,
+        icon: jqueryIcon,
         skill: "JQuery"
     },
     {
         id: 11,
-        icon: htmlIcon,
+        icon: csharpIcon,
         skill: "C#"
     },
     {
@@ -100,7 +108,16 @@ export const skills = [
         icon: htmlIcon,
         skill: "MS SQL"
     },
-
+    {
+        id: 14,
+        icon: htmlIcon,
+        skill: "Git"
+    },
+    {
+        id: 15,
+        icon: htmlIcon,
+        skill: "Git Hub"
+    },
 ]
 
 export const overviews = [
