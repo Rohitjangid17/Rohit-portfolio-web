be69de97847ecae110e20f0c7adbf65196931077
Rohitjangid17 <jangidrohit817@gmail.com>
2024-05-14 12:48:53 +0530

    sketch page add loader and hero section

--- src/components/Sketch.jsx
+++ src/components/Sketch.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Loader from '../components/Loader';
+import Hero from './Hero';
 
 function Sketch() {
+    const title = "Sketches";
+    const description = "Sketches";
+
     return (
         <>
+
+            <Hero title={title} description={description} iscontent={false} />
             <h1 className='text-5xl text-center py-5 font-bold'>Comming Soon</h1>
+            <Loader />
         </>
     )
 }
